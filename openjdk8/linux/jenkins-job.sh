013faaa19532b8e584faf371817c9a94806fbce1
Gomez Henri <henri.gomez@gmail.com>
2012-09-26 15:46:55 +0200

    Port jdk8-lambda port on build

--- openjdk8/linux/jenkins-job.sh
+++ openjdk8/linux/jenkins-job.sh
@@ -1,43 +1,80 @@
 #!/bin/bash
 #
 
-pushd sources
+export OBF_PROJECT_NAME=openjdk8
+TAG_FILTER=jdk8
 
-export HG_TAGS=`hg tags | grep jdk8 | head -1 | sed "s/jdk8//" | cut -d ' ' -f 1 | sed 's/^-//'`
-export JVM_VERSION=`echo $HG_TAGS | sed "s/-/./g"`
+#
+# Safe Environment
+#
+export LC_ALL=C
+export LANG=C
 
-echo "JVM_VERSION will be $JVM_VERSION"
+#
+# Prepare Drop DIR
+#
+export DROP_DIR="$HOME/DROP_DIR"
+mkdir -p $DROP_DIR
+
+#
+# Provide Main Variables to Scripts
+#
+if [ -z "$OBF_BUILD_PATH" ]; then
+  export OBF_BUILD_PATH=`pwd`/obuildfactory/$OBF_PROJECT_NAME/linux
+fi
+
+if [ -z "$OBF_SOURCES_PATH" ]; then
+  export OBF_SOURCES_PATH=`pwd`/sources
+fi
+
+pushd $OBF_SOURCES_PATH >>/dev/null
+
+#
+# OBF_MILESTONE will contains build tag number and name, ie b56 but without dash inside (suited for RPM packages)
+# OBF_BUILD_NUMBER will contains build number, ie b56
+# OBF_BUILD_DATE will contains build date, ie 20120908
+#
+# Build System concats OBF_MILESTONE, - and OBF_BUILD_DATE, ie b56-20120908
+#
+export OBF_MILESTONE=`hg tags | grep $TAG_FILTER | head -1 | cut -d ' ' -f 1 | sed 's/^-//'`
+export OBF_BUILD_NUMBER=`hg tags | grep $TAG_FILTER | head -1 | sed "s/$TAG_FILTER//" | cut -d ' ' -f 1 | sed 's/^-//'`
+export OBF_BUILD_DATE=`date +%Y%m%d`
+
+
+popd >>/dev/null
 
 if [ "$XBUILD" = "true" ]; then
-  ./linux/build.sh
-  
+  $OBF_BUILD_PATH/build.sh
+
   if [ $? != 0 ]; then
     exit -1
   fi
-  
+
 fi
 
 if [ "$XTEST" = "true" ]; then
-  pushd linux
-  ./test.sh
+  $OBF_BUILD_PATH/test.sh
 
   if [ $? != 0 ]; then
     exit -1
   fi
-
-  popd
 
 fi
 
 if [ "$XPACKAGE"  = "true" ]; then
-  pushd linux
-  ./package.sh
+  $OBF_BUILD_PATH/package.sh
 
   if [ $? != 0 ]; then
     exit -1
   fi
 
-  popd
 fi
 
-popd
+if [ "$XDEPLOY"  = "true" ]; then
+  $OBF_BUILD_PATH/deploy.sh
+
+  if [ $? != 0 ]; then
+    exit -1
+  fi
+
+fi
--- openjdk8/linux/test.sh
+++ openjdk8/linux/test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #
 
-PROJECT_NAME=openjdk8
+# Required vars :
+#
+# OBF_BUILD_PATH (absolute path of project build scripts, ie obuildfactory/openjdk8/linux)
+# OBF_SOURCES_PATH (absolute path of project sources)
+#
 
-export LC_ALL=C
-export LANG=C
-
-export DROP_DIR="$HOME/DROP_DIR"
-mkdir -p $DROP_DIR
+pushd $OBF_BUILD_PATH >>/dev/null
 
 echo "test later"
+
+popd >> /dev/null
