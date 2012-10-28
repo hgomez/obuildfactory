d6587faece7811f446d122058f946715fb24c717
Gomez Henri <henri.gomez@gmail.com>
2012-10-28 21:53:41 +0100

    back port from openjdk7

--- openjdk8/macosx/package.sh
+++ openjdk8/macosx/package.sh
@@ -40,55 +40,54 @@ DMG_BUNDLE_DIR=1.8.0.jdk
 #
 function build_dmg()
 {
-  PACKAGE_NAME=$1
-  BUNDLE_FILE=$2
-  SRC_BUNDLE=$3
-  DST_BUNDLE=$4
-  FILE_NAME=$5
+    PACKAGE_NAME=$1
+    BUNDLE_FILE=$2
+    SRC_BUNDLE=$3
+    DST_BUNDLE=$4
+    FILE_NAME=$5
   
-  pushd $OBF_BUILD_PATH/dmg >>/dev/null
+    pushd $OBF_BUILD_PATH/dmg >>/dev/null
 
-  rm -rf TEMP
-  mkdir -p TEMP
-
-  if [ -f $BUNDLE_FILE ]; then
-    echo "packaging $PACKAGE_NAME"
-
-    pushd TEMP >>/dev/null
-
-    tar xjf $BUNDLE_FILE
-    mv $SRC_BUNDLE $DST_BUNDLE
-    
-    # Set Milestone and buildnumber in Info.plist
-    sed -i "" -e "s|<string>$JVM_VERSION</string>|<string>$JVM_VERSION-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE</string>|" $DST_BUNDLE/Contents/Info.plist
-  
-    cp ../template.dmg.bz2 .
-    bzip2 -d template.dmg.bz2
-
-    DMG_MOUNT_DIR=`pwd`/MOUNT_DIR
-    mkdir -p $DMG_MOUNT_DIR
-
-    hdiutil attach template.dmg -readwrite -noverify -noautoopen -noautoopenro -noautoopenrw -noautofsck -noidme -noidmereveal -noidmetrash -mountpoint $DMG_MOUNT_DIR
-
-    rm -f $DMG_MOUNT_DIR/$DMG_BUNDLE_DIR
-    mv $DST_BUNDLE $DMG_MOUNT_DIR
-    cp -f ../README $DMG_MOUNT_DIR/README
-    cp -f ../LEGAL $DMG_MOUNT_DIR/LEGAL
-
-    ../SetFileIcon -image ../logo.png -file $DMG_MOUNT_DIR/$DST_BUNDLE
-
-    hdiutil detach $DMG_MOUNT_DIR -quiet -force
-    hdiutil convert template.dmg -format UDZO -imagekey zlib-level=9 -o $FILE_NAME-$OBF_BASE_ARCH-$PACKAGE_NAME-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE.dmg
-    rm template.dmg
-  
-    mkdir -p $OBF_DROP_DIR/$OBF_PROJECT_NAME
-    mv $FILE_NAME-$OBF_BASE_ARCH-$PACKAGE_NAME-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE.dmg $OBF_DROP_DIR/$OBF_PROJECT_NAME
-  
-    popd >>/dev/null
     rm -rf TEMP
-  else
-    echo "missing $PACKAGE_NAME bundle tarball $BUNDLE_FILE, skipping packaging"
-  fi
+    mkdir -p TEMP
+
+    if [ -f $BUNDLE_FILE ]; then
+      echo "packaging $PACKAGE_NAME"
+
+      pushd TEMP >>/dev/null
+
+      tar xjf $BUNDLE_FILE
+    
+      # Set Milestone and buildnumber in Info.plist
+      sed -i "" -e "s|<string>$JVM_VERSION</string>|<string>$JVM_VERSION-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE</string>|" $SRC_BUNDLE/Contents/Info.plist
+  
+      cp ../template.dmg.bz2 .
+      bzip2 -d template.dmg.bz2
+
+      DMG_MOUNT_DIR=`pwd`/MOUNT_DIR
+      mkdir -p $DMG_MOUNT_DIR
+
+      hdiutil attach template.dmg -readwrite -noverify -noautoopen -noautoopenro -noautoopenrw -noautofsck -noidme -noidmereveal -noidmetrash -mountpoint $DMG_MOUNT_DIR
+
+      rm -f $DMG_MOUNT_DIR/$DMG_BUNDLE_DIR
+      mv $SRC_BUNDLE $DMG_MOUNT_DIR/$DST_BUNDLE
+      cp -f ../README $DMG_MOUNT_DIR/README
+      cp -f ../LEGAL $DMG_MOUNT_DIR/LEGAL
+
+      ../SetFileIcon -image ../logo.png -file $DMG_MOUNT_DIR/$DST_BUNDLE
+
+      hdiutil detach $DMG_MOUNT_DIR -quiet -force
+      hdiutil convert template.dmg -format UDZO -imagekey zlib-level=9 -o $FILE_NAME-$OBF_BASE_ARCH-$PACKAGE_NAME-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE.dmg
+      rm template.dmg
+  
+      mkdir -p $OBF_DROP_DIR/$OBF_PROJECT_NAME
+      mv $FILE_NAME-$OBF_BASE_ARCH-$PACKAGE_NAME-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE.dmg $OBF_DROP_DIR/$OBF_PROJECT_NAME
+  
+      popd >>/dev/null
+      rm -rf TEMP
+    else
+      echo "missing $PACKAGE_NAME bundle tarball $BUNDLE_FILE, skipping packaging"
+    fi
 }
 
 # jdk/jre
