79cdcd9fcf8725fed1328c4de3c34104dd41a6af
Gomez Henri <henri.gomez@gmail.com>
2013-02-05 14:44:08 +0100

    no milestone or build-number configure options in Jigsaw yet

--- openjdk8-jigsaw/linux/build.sh
+++ openjdk8-jigsaw/linux/build.sh
@@ -207,8 +207,7 @@ function build_new()
   rm -rf $OBF_WORKSPACE_PATH/.ccache
   mkdir -p $OBF_WORKSPACE_PATH/.ccache
 
-  sh ../autoconf/configure --with-boot-jdk=$OBF_BOOTDIR --with-freetype=$OBF_FREETYPE_DIR --with-cacerts-file=$OBF_DROP_DIR/cacerts --with-ccache-dir=$OBF_WORKSPACE_PATH/.ccache \
-                           --with-build-number=$OBF_BUILD_DATE --with-milestone=$OBF_MILESTONE
+  sh ../autoconf/configure --with-boot-jdk=$OBF_BOOTDIR --with-freetype=$OBF_FREETYPE_DIR --with-cacerts-file=$OBF_DROP_DIR/cacerts --with-ccache-dir=$OBF_WORKSPACE_PATH/.ccache 
   make images
 
   # restore original common/autoconf/version.numbers
