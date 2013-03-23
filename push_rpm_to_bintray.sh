bc4ed27165db6aab24a92431e4a425b6d595f004
Gomez Henri <henri.gomez@gmail.com>
2013-03-23 15:09:30 +0100

    update message

--- push_rpm_to_bintray.sh
+++ push_rpm_to_bintray.sh
@@ -39,30 +39,33 @@ fi
 echo "RPM_NAME=$RPM_NAME, RPM_VERSION=$RPM_VERSION, RPM_RELEASE=$RPM_RELEASE, RPM_ARCH=$RPM_ARCH"
 echo "BINTRAY_USER=$BINTRAY_USER, BINTRAY_APIKEY=$BINTRAY_APIKEY, BINTRAY_REPO=$BINTRAY_REPO, RPM_FILE=$RPM_FILE, BASE_DESC=$BASE_DESC"
 
-echo "@@@@@@@@@@@@@@@@@@@@@@"
-echo "@@@ delete package @@@"
-echo "@@@@@@@@@@@@@@@@@@@@@@"
+echo "Deleting package from Bintray.."
 HTTP_CODE=`$CURL_CMD -H "Content-Type: application/json" -X DELETE https://api.bintray.com/packages/$BINTRAY_ACCOUNT/$BINTRAY_REPO/$RPM_NAME`
-echo "delete package -> $HTTP_CODE"
 
-echo "@@@@@@@@@@@@@@@@@@@@@@"
-echo "@@@ create package @@@"
-echo "@@@@@@@@@@@@@@@@@@@@@@"
+if [ "$HTTP_CODE" != "200" ]; then
+ echo "can't delete package -> $HTTP_CODE"
+else
+ echo "Package deleted"
+fi
+
+echo "Creating package on Bintray.."
 HTTP_CODE=`$CURL_CMD -H "Content-Type: application/json" -X POST https://api.bintray.com/packages/$BINTRAY_ACCOUNT/$BINTRAY_REPO/ --data "{ \"name\": \"$RPM_NAME\", \"desc\": \"${RPM_DESCRIPTION}\", \"desc_url\": \"$DESC_URL\", \"labels\": \"\" }"`
 
 if [ "$HTTP_CODE" != "201" ]; then
  echo "can't create package -> $HTTP_CODE"
  exit -1
+else
+ echo "Package created"
 fi
 
-echo "@@@@@@@@@@@@@@@@@@@@@@"
-echo "@@@ upload content @@@"
-echo "@@@@@@@@@@@@@@@@@@@@@@"
+echo "Uploading package to Bintray.."
 HTTP_CODE=`$CURL_CMD -T $RPM_FILE -u$BINTRAY_USER:$BINTRAY_APIKEY -H "X-Bintray-Package:$RPM_NAME" -H "X-Bintray-Version:$RPM_VERSION-$RPM_RELEASE" "https://api.bintray.com/content/$BINTRAY_ACCOUNT/$BINTRAY_REPO/$REPO_FILE_PATH;publish=1"`
 
 if [ "$HTTP_CODE" != "201" ]; then
  echo "failed to upload package -> $HTTP_CODE"
  exit -1
+else
+ echo "Package uploaded"
 fi
 
 exit 0
