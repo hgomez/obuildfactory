54dff9a5036b97d37224381bd732f885403afce7
Matthias Bläsing <mblaesing@doppel-helix.eu>
2014-03-24 20:15:50 +0100

    Implement building openjdk8 with openjfx8

---
+++ openjdk8-javafx/linux/deploy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+
+# Required vars :
+#
+# OBF_BUILD_PATH (absolute path of project, ie obuildfactory/openjdk8-lambda/linux)
+#
+# OBF_BASE_URL (ie: http://www.obuildfactory.org)
+# OBF_BASE_PATH (ie: /home/jenkinsa/www)
+# OBF_UPLOAD_PATH (ie: /home/jenkinsa/upload)
+#
+# OBF_BASE_ARCH (i386, x86_64, or ppc64)
+# OBF_DISTRIBUTION (centos, fedora, opensuse...)
+# OBF_RELEASE_VERSION (5, 6, 121)
+#
+# OBF_GPG_ID (ie: packagers@obuildfactory.org)
+# OBF_GPG_PASSWORD (ie: 123456)
+# OBF_UPLOADER_USER_ID (ie: henri)
+# OBF_UPLOAD_HOST (ie: packages.obuildfactory.org)
+
+DEST_ARCH=$OBF_BASE_ARCH
+
+if [ "$OBF_DISTRIBUTION" = "opensuse" ]; then
+  if [ "$OBF_BASE_ARCH" = "i386" ]; then
+    DEST_ARCH=i586
+  fi
+fi
+
+UPLOAD_DIR=$OBF_UPLOAD_PATH/$OBF_DISTRIBUTION/$OBF_RELEASE_VERSION/$DEST_ARCH
+YUM_REPO_DIR=$OBF_BASE_PATH/$OBF_DISTRIBUTION/$OBF_RELEASE_VERSION/$DEST_ARCH
+YUM_INDEX_DIR=$YUM_REPO_DIR
+
+if [ "$OBF_DISTRIBUTION" = "opensuse" ]; then
+  YUM_INDEX_DIR=$OBF_BASE_PATH/$OBF_DISTRIBUTION/$OBF_RELEASE_VERSION
+fi
+
+echo "### signing RPMS ###"
+for RPM_FILE in $OBF_BUILD_PATH/rpm/RPMS/*/*.rpm; do
+  $OBF_BUILD_PATH/rpmsign-batch.expect $OBF_GPG_ID $OBF_GPG_PASSWORD $RPM_FILE
+done
+
+[ "$OBF_DISTRIBUTION" = "centos" ] && [ "$OBF_RELEASE_VERSION" = "5" ] && CREATE_REPO_OPT="-s sha1"
+
+echo "### creating upload directory ###"
+ssh $OBF_UPLOADER_USER_ID@$OBF_UPLOAD_HOST -o StrictHostKeyChecking=no "mkdir -p $UPLOAD_DIR"
+
+echo "### copying RPMs to upload directory $UPLOAD_DIR ###"
+scp $OBF_BUILD_PATH/rpm/RPMS/*/*.rpm $OBF_UPLOADER_USER_ID@$OBF_UPLOAD_HOST:$UPLOAD_DIR
+echo "### moving RPMs from upload directory to final destination ###"
+ssh $OBF_UPLOADER_USER_ID@$OBF_UPLOAD_HOST "mkdir -p $YUM_REPO_DIR"
+ssh $OBF_UPLOADER_USER_ID@$OBF_UPLOAD_HOST "mv $UPLOAD_DIR/* $YUM_REPO_DIR"
+
+echo "### reindexing YUM repository ###"
+ssh $OBF_UPLOADER_USER_ID@$OBF_UPLOAD_HOST "createrepo --update $CREATE_REPO_OPT $YUM_INDEX_DIR"
+echo "### signing repomd.xml ###"
+ssh $OBF_UPLOADER_USER_ID@$OBF_UPLOAD_HOST "gpg --yes --batch --passphrase=$OBF_GPG_PASSWORD --armor --sign $YUM_INDEX_DIR/repodata/repomd.xml" 
---
+++ openjdk8-javafx/linux/standalone-job.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+
+export OBF_PROJECT_NAME=openjdk8-javafx
+TAG_FILTER=jdk8
+
+#
+# Safe Environment
+#
+LC_ALL=C
+LANG=C
+LANGUAGE=C
+export LC_ALL
+export LANG
+export LANGUAGE
+
+#
+# Prepare Drop DIR
+#
+if [ -z "$OBF_DROP_DIR" ]; then
+  export OBF_DROP_DIR=`pwd`/OBF_DROP_DIR;
+fi
+
+#
+# Provide Main Variables to Scripts
+#
+if [ -z "$OBF_BUILD_PATH" ]; then
+  export OBF_BUILD_PATH=`pwd`/obuildfactory/$OBF_PROJECT_NAME/linux;
+fi
+
+echo  $OBF_BUILD_PATH;
+
+if [ -z "$OBF_SOURCES_PATH" ]; then
+  export OBF_SOURCES_PATH=`pwd`/sources/$OBF_PROJECT_NAME
+  mkdir -p `pwd`/sources
+fi
+
+if [ -z "$OBF_WORKSPACE_PATH" ]; then
+  export OBF_WORKSPACE_PATH=`pwd`
+fi
+
+if [ ! -d $OBF_SOURCES_PATH ]; then
+  hg clone http://hg.openjdk.java.net/jdk8/jdk8 $OBF_SOURCES_PATH
+  hg clone http://hg.openjdk.java.net/openjfx/8u-dev/rt $OBF_SOURCES_PATH-openjfx
+  cd $OBF_SOURCES_PATH-openjfx
+  hg revert -a
+  cd $DIR
+fi	
+	
+pushd $OBF_SOURCES_PATH >>/dev/null
+
+# 
+# Updating sources for Mercurial repo
+#
+sh ./get_source.sh
+
+#
+# Update sources to provided tag XUSE_TAG (if defined)
+#
+if [ ! -z "$XUSE_TAG" ]; then
+  echo "using tag $XUSE_TAG"
+  sh ./make/scripts/hgforest.sh update $XUSE_TAG
+fi
+
+if [ ! -z "$XUSE_JFX_TAG" ]; then
+  echo "using javafx tag $XUSE_JFX_TAG"
+  DIR=`pwd`
+  cd $OBF_SOURCES_PATH-openjfx
+  hg checkout $XUSE_JFX_TAG
+  cd $DIR
+fi
+
+#
+# Apply patches
+#
+patch -d $OBF_SOURCES_PATH-openjfx -p1 < $OBF_BUILD_PATH/patches/build.patch
+echo -e "COMPILE_WEBKIT = true\nCOMPILE_MEDIA = true" > $OBF_SOURCES_PATH-openjfx/gradle.properties
+
+#
+# OBF_MILESTONE will contains build tag number and name, ie b56 but without dash inside (suited for RPM packages)
+# OBF_BUILD_NUMBER will contains build number, ie b56
+# OBF_BUILD_DATE will contains build date, ie 20120908
+#
+# Build System concats OBF_MILESTONE, - and OBF_BUILD_DATE, ie b56-20120908
+#
+export OBF_MILESTONE=`hg tags | grep $TAG_FILTER | head -1 | cut -d ' ' -f 1 | sed 's/^-//'`
+export OBF_BUILD_NUMBER=`hg tags | grep $TAG_FILTER | head -1 | sed "s/$TAG_FILTER//" | cut -d ' ' -f 1 | sed 's/^-//'`
+export OBF_BUILD_DATE=`date +%Y%m%d`
+
+popd >>/dev/null
+
+#
+# Mercurial repositories updated, call Jenkins job now
+#
+$OBF_BUILD_PATH/jenkins-job.sh
+
+cd $OBF_SOURCES_PATH-openjfx
+hg revert -a
+rm gradle.properties
+cd $DIR
+
