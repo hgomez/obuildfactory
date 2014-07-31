af035c120e612ef914655030e2f3efa70a082306
Henri Gomez <henri.gomez@gmail.com>
2014-07-31 13:17:47 +0200

    fastdebug mode package

--- openjdk8/linux/package.sh
+++ openjdk8/linux/package.sh
@@ -82,7 +82,9 @@ else
         echo "packaging JDK"
         cp $OBF_DROP_DIR/$OBF_PROJECT_NAME/j2sdk-image$FILENAME_PREFIX-$OBF_BASE_ARCH-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE.tar.bz2 SOURCES/j2sdk-image.tar.bz2
 
-        rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="jvm_version $OBF_BUILD_NUMBER" --define="jdk_model $OBF_JDK_MODEL$FILENAME_PREFIX" --define="cum_jdk 0" SPECS/jdk.spec
+        rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="jvm_version $OBF_BUILD_NUMBER" \
+                     --define="jdk_type $FILENAME_PREFIX" --define="jdk_model $OBF_JDK_MODEL" --define="cum_jdk 0" \
+                     SPECS/jdk.spec
 
         if [ $? != 0 ]; then
             exit -1
@@ -97,7 +99,9 @@ else
         echo "packaging JRE"
         cp $OBF_DROP_DIR/$OBF_PROJECT_NAME/j2re-image$FILENAME_PREFIX-$OBF_BASE_ARCH-$OBF_BUILD_NUMBER-$OBF_BUILD_DATE.tar.bz2 SOURCES/j2re-image.tar.bz2
 
-        rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="jvm_version $OBF_BUILD_NUMBER" --define="jdk_model $OBF_JDK_MODEL$FILENAME_PREFIX" --define="cum_jdk 0" SPECS/jre.spec
+        rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="jvm_version $OBF_BUILD_NUMBER" \
+                     --define="jdk_type $FILENAME_PREFIX" --define="jdk_model $OBF_JDK_MODEL" --define="cum_jdk 0" \
+                     SPECS/jre.spec
 
         if [ $? != 0 ]; then
             exit -1
