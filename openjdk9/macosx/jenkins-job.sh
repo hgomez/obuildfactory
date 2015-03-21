f64218833bb9605cb7139c7ec182597920c1f043
Jakub Jirutka <jakub@jirutka.cz>
2015-03-21 23:32:03 +0100

    Fix calculation of milestone [OS X]
    Ported from 404e7fd0e062bf2f0768fd7e0abf1e7ffb69b07c and
    8f95c153fbd7843abd182619a80cfb2ce0995c2e

--- openjdk9/macosx/jenkins-job.sh
+++ openjdk9/macosx/jenkins-job.sh
@@ -47,8 +47,12 @@ pushd $OBF_SOURCES_PATH >>/dev/null
 #
 # Build System concats OBF_MILESTONE, - and OBF_BUILD_DATE, ie b56-20120908
 #
-export OBF_MILESTONE=`hg tags | grep $TAG_FILTER | head -1 | cut -d ' ' -f 1 | sed 's/^-//'`
-export OBF_BUILD_NUMBER=`hg tags | grep $TAG_FILTER | head -1 | sed "s/$TAG_FILTER//" | cut -d ' ' -f 1 | sed 's/^-//'`
+export OBF_MILESTONE=`hg identify | cut -d ' ' -f 2 | cut -d '/' -f 1`
+if [ "$OBF_MILESTONE" = "tip" ]; then
+  export OBF_MILESTONE=`hg tags | grep $TAG_FILTER | head -1 | cut -d ' ' -f 1`
+fi
+
+export OBF_BUILD_NUMBER=`echo $OBF_MILESTONE | sed "s/$TAG_FILTER//" | sed 's/^-//'`
 export OBF_BUILD_DATE=`date +%Y%m%d`
 
 if [ -z "$OBF_DISTRIBUTION" ]; then
